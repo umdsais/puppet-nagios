166bf3e3a1e4f07047995af67233606f4e8e09a7
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2017-08-17 15:27:03 +0100

    Remove excess params

--- manifests/client.pp
+++ manifests/client.pp
@@ -4,11 +4,9 @@ class nagios::client (
   $nsca                = true,
   $selinux             = true,
   $firewall            = true,
-  $url                 = $::fqdn,
   $nrpe_package        = $nagios::params::nrpe_package,
   $webroot             = $nagios::params::webroot,
   $cgiroot             = $nagios::params::cgiroot,
-  $dev                 = false,
   $nsca_client_package = $nagios::params::nsca_client_package,
   $nrpe_service        = $nagios::params::nrpe_service,
   $nrpe_config         = $nagios::params::nrpe_config,
@@ -17,13 +15,6 @@ class nagios::client (
   $nsca_server_package = $nagios::params::nsca_server_package,
   $nsca_service        = $nagios::params::nsca_service,
   $nsca_config         = $nagios::params::nsca_config,
-  $nagios_package      = $nagios::params::nagios_package,
-  $nagios_service      = $nagios::params::nagios_service,
-  $serveradmin         = 'root@localhost',
-  $ssl_cert            = '/path/to/cert.crt',
-  $ssl_key             = '/path/to/key.key',
-  $ssl_chain           = undef,
-  $auth_type           = 'basic',
 ) inherits nagios::params {
 
   if ($nsca) {
