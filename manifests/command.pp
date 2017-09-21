f86b25ce2f383c805de7d8354b02889eeef7148f
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2017-09-21 16:36:37 +0100

    Strip of command suffixes after the exclamation mark

--- manifests/command.pp
+++ manifests/command.pp
@@ -4,7 +4,11 @@ define nagios::command (
   $command_name,
   $command_line,
 ) {
-  ensure_resource('nagios_command', $command_name, {
+
+  # Strip off everything after the !, e.g. check_ping!192.168.0.1
+  $trunc_command_name = regsubst($command_name, '^(.*)!?.*$', '\1')
+
+  ensure_resource('nagios_command', $trunc_command_name, {
     'command_line' => $command_line,
     'ensure'       => 'present',
     'owner'        => 'root',
